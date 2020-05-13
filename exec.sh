249f4bdc687db876cdaf92922664a4b31da0380d
Aurélien Besnier <aurelienbesnier7320@gmail.com>
2020-05-13 10:21:16 +0200

    Fix Java path exec.sh

--- exec.sh
+++ exec.sh
@@ -2,7 +2,7 @@
 if [ $# != 3 ]
  then
     echo "Mauvaise utilisation du fichier, la syntaxe est : $0 nb_excecution_des_progammes fichier_a_parcourir sequence_a_trouver"
-    exit 1 
+    exit 1
 
  else
     if [ ! -d "./Results" ]; then
@@ -13,14 +13,14 @@ if [ $# != 3 ]
     echo "Execution de la version C++"
     make -f ./C++/MakefileExec
     ./C++/kmp $1 -f $2 $3 "./Results/result-cpp.data"
-    make clean -f ./C++/MakefileExec 
+    make clean -f ./C++/MakefileExec
     echo "Execution de la version Python"
     ./Python/main.py "$3" "$2" "$1" "./Results/result-py.data"
     #execution de la version Java
     echo "Execution Java"
     javac ./Java/KmpAlgorithme.java
     cd ./Java
-    java KmpAlgorithme $1 ".$2" $3
+    java KmpAlgorithme $1 "../$2" $3
     cd ..
     echo "Recupération de la moyenne des résultats obtenus."
     cat ./Results/*.data > ./Results/all.data
